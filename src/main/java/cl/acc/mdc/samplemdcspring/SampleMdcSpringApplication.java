a1a3d55901e421ad89663db1a1af77703e8ba421
Alexander Castillo <alcastic.dev@gmail.com>
2018-08-29 15:06:47 -0300

    Add initial spring framework conf.

---
+++ src/main/java/cl/acc/mdc/samplemdcspring/SampleMdcSpringApplication.java
@@ -0,0 +1,12 @@
+package cl.acc.mdc.samplemdcspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMdcSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMdcSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/cl/acc/mdc/samplemdcspring/SampleMdcSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package cl.acc.mdc.samplemdcspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleMdcSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
